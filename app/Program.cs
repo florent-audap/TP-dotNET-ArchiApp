d377892ccb3345e88af1e61f064c7059a9f3d0e3
Florent Audap <134688107+florent-audap@users.noreply.github.com>
2026-03-27 13:38:37 +0000

    Part 1 done

---
+++ app/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace app;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Person p = new Person { Nom = "Alice", Age = 30 };
+
+        string message = p.Hello(true);
+        Console.WriteLine(message);
+    }
+};
+
+
+class Person
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+    
+    // Méthode Hello avec logique conditionnelle
+    public string Hello(bool isLowercase)
+    {
+        string resultat = $"hello {Nom}, you are {Age}";
+
+        if (isLowercase)
+        {
+            return resultat;
+        }
+        else
+        {
+            return resultat.ToUpper();
+        }
+    }
+}
